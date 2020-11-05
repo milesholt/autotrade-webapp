4095511c138afcfd8b0ec1e2d3aef4e3b6f4ec34
Miles Holt <contact@milesholt.co.uk>
2020-11-05 18:49:38 +0000

    Updated webapp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -15,8 +15,16 @@ export class AppComponent {
   title = 'autotrade-web';
   data:any = {}
 
+//   private httpOptions = {
+//   headers: new HttpHeaders({
+//     'Accept': 'text/html, application/xhtml+xml, */*',
+//     'Content-Type': 'application/x-www-form-urlencoded'
+//   }),
+//   responseType: 'text'
+// };
+
   constructor(private http: HttpClient){
-  } 
+  }
 
   ngOnInit(){
   }
@@ -27,8 +35,18 @@ export class AppComponent {
   }
 
   getData(){
-    let url = 'https://raw.githubusercontent.com/milesholt/autotrade1/version2/core/data/CS.D.XLMUSD.TODAY.IP_pricedata.json';
-    this.http.get(url).subscribe((res) => this.data = res);
+    setInterval(() => {
+      let url = 'https://raw.githubusercontent.com/milesholt/autotrade1/version2/core/data/CS.D.XLMUSD.TODAY.IP/CS.D.XLMUSD.TODAY.IP_streamdata.json';
+      this.http.get(url, {responseType: 'text'}).subscribe((res) =>{
+
+         let buff = new Buffer.from(res, 'base64');
+         let string = buff.toString('ascii');
+         let obj = JSON.parse(string);
+         console.log(obj);
+         this.data = obj;
+      });
+    },10000);
+
   }
 
 }
