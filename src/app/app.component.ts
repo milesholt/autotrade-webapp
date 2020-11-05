247f343a8a1837e4a0f4e247a1ffefe3520096b3
Miles Holt <contact@milesholt.co.uk>
2020-11-05 17:27:25 +0000

    Updated webapp to get JSON data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+
+@Injectable()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'autotrade-web';
+  data:any = {}
+
+  constructor(private http: HttpClient){
+  } 
+
+  ngOnInit(){
+  }
+
+  ngAfterViewInit(){
+      console.log('Loaded web app. Getting data...');
+      this.getData();
+  }
+
+  getData(){
+    let url = 'https://raw.githubusercontent.com/milesholt/autotrade1/version2/core/data/CS.D.XLMUSD.TODAY.IP_pricedata.json';
+    this.http.get(url).subscribe((res) => this.data = res);
+  }
+
 }
