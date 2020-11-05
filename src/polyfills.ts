ef5b38155eff7de6183b82a56cb86d55cfae440c
Miles Holt <contact@milesholt.co.uk>
2020-11-05 19:25:55 +0000

    Added method to create require

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,6 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
